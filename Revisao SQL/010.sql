634240d878720e23dd36dfcca3c972812a20a91d
fleyhe <fleyhe0@gmail.com>
2016-11-23 12:09:25 -0200

    Trab SQL 28/11/16

--- Revisao SQL/010.sql
+++ Revisao SQL/010.sql
@@ -2,6 +2,11 @@
 (
 	select * from copias cp where not exists
 	(
-		select * from locacoes l where l.id = cp.id and l.filme = cp.filme and l.cliente = c.id and cp.midia = 'VHS' and cp.tipo = 'legendado'
+		select * from locacoes l
+		where l.id = cp.id
+		and l.filme = cp.filme
+		and l.cliente = c.id
+		and cp.midia = 'VHS'
+		and cp.tipo = 'legendado'
 	)
-)
+) and c.cidade = 'Florianópolis'
